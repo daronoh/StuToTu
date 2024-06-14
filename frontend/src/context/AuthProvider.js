53842380aa1d15205320fe8b7c35a0a7f85cd749
anys <163435046+lilcici@users.noreply.github.com>
2024-06-14 13:56:52 +0800

    update authprovider

--- frontend/src/context/AuthProvider.js
+++ frontend/src/context/AuthProvider.js
@@ -1,9 +1,42 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import axios from '../api/axios';
 
 const AuthContext = createContext({});
 
 export const AuthProvider = ({ children }) => {
-    const [auth, setAuth] = useState({});
+    const [auth, setAuth] = useState(() => {
+        const token = localStorage.getItem('token');
+        return token ? { token } : {};
+    });
+
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            setAuth({ token });
+        }
+    }, []);
+
+    const setToken = (token) => {
+        localStorage.setItem('token', token);
+        setAuth({ token });
+    };
+
+    const getToken = () => {
+        return localStorage.getItem('token');
+    };
+
+    axios.interceptors.request.use(
+        config => {
+            const token = getToken();
+            if (token) {
+                config.headers['Authorization'] = `Bearer ${token}`;
+            }
+            return config;
+        },
+        error => {
+            return Promise.reject(error);
+        }
+    );
 
     return (
         <AuthContext.Provider value = {{ auth, setAuth}}>
