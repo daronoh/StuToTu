77673aeaf2de2e1e68f4d1d81682cb1d7502036e
anys <163435046+lilcici@users.noreply.github.com>
2024-06-30 22:40:54 +0800

    profile update

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -48,6 +48,10 @@ const Navbar = () => {
               <button onClick={() => navigate('/calendar')} className="link-button">Shared Calendar</button>
             </li>
             <li>
+              <button onClick={() => navigate(`/profile/edit/${user}`)} className="link-button">Settings</button>
+            </li>
+            
+            <li>
               <button onClick={handleLogout} className="logout-button">Logout</button>
             </li>
         </ul>
@@ -57,6 +61,7 @@ const Navbar = () => {
           <button onClick={() => navigate('/home')} className="link-button">Home</button>
           <button onClick={handleProfile} className="link-button">My Profile</button>
           <button onClick={() => navigate('/calendar')} className="link-button">Shared Calendar</button>
+          <button onClick={() => navigate(`/profile/edit/${user}`)} className="link-button">Settings</button>
           <button onClick={handleLogout}>Logout</button>
       </div>
     </div>
