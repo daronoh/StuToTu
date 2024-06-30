5e538203559f49e1ce1b45d05b074b7429f1bb1a
anys <163435046+lilcici@users.noreply.github.com>
2024-06-30 22:52:44 +0800

    update profile.js

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -51,9 +51,16 @@ const Profile = () => {
 
     return (
         <div className='body-content'>
-            {getRole() !== profileData.role && !isFriend && (
+            {getRole() !== profileData.role && !isFriend ? (
                 <AddAsFriendButton requestData={{requester: getUser(), receiver: profileData.username}}/>
-            )}
+            ) : getUser() !== profileData.username ? (
+                <Button
+                    variant="contained"
+                    color="primary"
+                    style={{ top: 200, right: 100, position: 'absolute' }}
+                    disabled="true"
+                >Already added as Friend</Button>
+            ) : (<></>)}
 
             <Grid container spacing={0} className='centered-container'>
                 <Grid item xs={12}>
